e40bd97c834e8923cfb31c7ee11b40a7f908c671
AB Noori <ab@ABs-MacBook-Pro.local>
2020-11-27 23:40:55 -0800

    broadcast connects/disconnects users

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('./node_modules/socket.io')(http);
 var port = process.env.PORT || 3000;
+
+let onlineUsers = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+  socket.on('new user', function(username) {
+    //Save the username as key to access the user's socket id
+    onlineUsers[username] = socket.id;
+    //Save the username to socket as well. This is important for later.
+    socket["username"] = username;
+    console.log(`✋ ${username} has joined the chat! ✋`);
+    io.emit("new user", username, {
+      // channels : channels,
+    });
+  })
+
+  socket.on('disconnect', () => {
+    //This deletes the user by using the username we saved to the socket
+    delete onlineUsers[socket.username]
+    io.emit('user has left', socket.username);
+  });
+
 });
 
 http.listen(port, function(){
